40cb21ef70c8f02b04896c5621e557b0bfcd4fbc
Yukon Colegrove-Winkfield <1003597ywinkfield@gmail.com>
2023-09-25 12:39:45 -0400

    generic commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PokeTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeInfo meh = new TypeInfo();
+            meh.Instance();
+        }
+    }
+}
---
+++ UI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeTypes
+{
+    public class UI
+    {
+        public void EnterType()
+        {
+            Console.Write("Please enter type here: ");
+            Console.ReadLine();
+        }
+        public void HiWelcome()
+        {
+            Console.WriteLine("Hi! Welcome!");
+        }
+    }
+}
